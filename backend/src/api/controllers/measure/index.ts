ce47c79b674aaaa2f664d412553956dc0ed66586
Misael Alberto <misael.alberto@outlook.com>
2024-08-29 16:43:26 -0300

    ajustes na validação

--- backend/src/api/controllers/measure/index.ts
+++ backend/src/api/controllers/measure/index.ts
@@ -11,22 +11,22 @@ export const validateData = async (req: Request): Promise<Validate> => {
 
   if (error) {
     // Se houver erros, retorna uma resposta com status 400 e os detalhes dos erros  
-    return new Validate({      
+    return new Validate({
       message: 'Erro de validação',
       details: error.details.map(err => err.message),
-      error: true    
+      error: true
     })
   }
 
   // Validar o tipo de dados dos parâmetros enviados (inclusive o base64)
-  if (validateBase64(req.body.image)) {    
+  if (validateBase64(req.body.image)) {
     return new Validate({
       message: 'Erro na imagem',
       details: ['Erro na imagem'],
       error: true
     })
   }
-    
+
   return new Validate({
     message: 'Sucesso',
     details: [''],
