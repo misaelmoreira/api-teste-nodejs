07a7f1a3400b933b2f0aacc32894213270c30ce5
Misael Alberto <misael.alberto@outlook.com>
2024-08-29 16:04:44 -0300

    adicionando o modelo measure

--- backend/src/db/init.ts
+++ backend/src/db/init.ts
@@ -1,8 +1,10 @@
 import Customer from './models/customer';
+import Measure from './models/measure';
 const isDev = process.env.NODE_ENV === 'development'
 
 const dbInit = async () => {
     await Customer.sync({ alter: isDev })
+    await Measure.sync({ alter: isDev })    
 };
 
 export default dbInit 
--- backend/src/db/models/measure.ts
+++ backend/src/db/models/measure.ts
@@ -1,36 +1,79 @@
-interface Medida {
-    measure_uuid: string;
-    measure_datetime: Date;
-    measure_type: string;
-    has_confirmed: boolean;
-    image_url: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    deletedAt?: Date;
+import { DataTypes, Model, Optional } from 'sequelize';
+import sequelize from '../config'
+
+interface MeasureAttributes {
+  measure_uuid?: string;
+  measure_datetime: Date;
+  measure_type: string;
+  measure_value: number;
+  has_confirmed: boolean;
+  image_url: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+  customer_id: number;
+}
+
+export interface MeasureInput extends Optional<MeasureAttributes, 'measure_uuid'> {}
+export interface MeasureOuput extends Required<MeasureAttributes> {}
+
+
+class Measure extends Model<MeasureAttributes> implements MeasureAttributes {
+  public measure_uuid?: string
+  public measure_datetime!: Date
+  public measure_type!: string
+  public measure_value!: number
+  public has_confirmed!: boolean
+  public image_url!: string
+  public customer_id!: number
+
+  // timestamps!
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+  public readonly deletedAt!: Date;
+
+  // Associações
+  static associate(models: any) {
+    Measure.belongsTo(models.Customer, { foreignKey: 'customer_id', as: 'customer' });
   }
-  
-  class Measure implements Medida {
-    measure_uuid: string;
-    measure_datetime: Date;
-    measure_type: string;
-    has_confirmed: boolean;
-    image_url: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-    deletedAt?: Date;
-  
-    constructor(data: Medida) {
-      this.measure_uuid = data.measure_uuid;
-      this.measure_datetime = data.measure_datetime;
-      this.measure_type = data.measure_type;
-      this.has_confirmed = data.has_confirmed;
-      this.image_url = data.image_url;
-      this.createdAt = data.createdAt;
-      this.updatedAt = data.updatedAt;
-      this.deletedAt = data.deletedAt;
+}
+
+Measure.init({
+  measure_uuid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true,
+  },
+  measure_datetime: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  measure_type: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },  
+  measure_value: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  has_confirmed: {
+    type: DataTypes.BOOLEAN
+  },
+  image_url: {
+    type: DataTypes.STRING
+  },
+  customer_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'Customers', 
+      key: 'id', 
     }
-  
-    // Métodos adicionais podem ser adicionados aqui
   }
-  
-  export default Measure
+}, {
+  timestamps: true,
+  sequelize: sequelize,
+  paranoid: true
+})
+
+export default Measure
